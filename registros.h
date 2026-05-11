473e940077230b1f8e823ce962889ec407a74a7e
Alexis Meneses <alexjose70@gmail.com>
2026-05-11 15:45:41 -0500

    adding view

---
+++ registros.h
@@ -0,0 +1,201 @@
+#pragma once
+
+namespace GUI {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for registros
+	/// </summary>
+	public ref class registros : public System::Windows::Forms::Form
+	{
+	public:
+		registros(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~registros()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+
+
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ id;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Nombre;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Precio;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->id = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Nombre = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Precio = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(293, 117);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(114, 32);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Nombre";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(446, 114);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(160, 38);
+			this->textBox1->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(827, 106);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(178, 52);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Agregar";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->AllowUserToAddRows = false;
+			this->dataGridView1->AllowUserToDeleteRows = false;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(3) {
+				this->id, this->Nombre,
+					this->Precio
+			});
+			this->dataGridView1->Location = System::Drawing::Point(104, 340);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->ReadOnly = true;
+			this->dataGridView1->RowHeadersWidth = 102;
+			this->dataGridView1->RowTemplate->Height = 40;
+			this->dataGridView1->Size = System::Drawing::Size(918, 149);
+			this->dataGridView1->TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(446, 182);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(160, 38);
+			this->textBox2->TabIndex = 5;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(293, 185);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(238, 80);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Precio";
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(827, 174);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(178, 52);
+			this->button2->TabIndex = 6;
+			this->button2->Text = L"Editar";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// id
+			// 
+			this->id->HeaderText = L"id";
+			this->id->MinimumWidth = 12;
+			this->id->Name = L"id";
+			this->id->ReadOnly = true;
+			this->id->Width = 250;
+			// 
+			// Nombre
+			// 
+			this->Nombre->HeaderText = L"Nombre";
+			this->Nombre->MinimumWidth = 12;
+			this->Nombre->Name = L"Nombre";
+			this->Nombre->ReadOnly = true;
+			this->Nombre->Width = 250;
+			// 
+			// Precio
+			// 
+			this->Precio->HeaderText = L"Precio";
+			this->Precio->MinimumWidth = 12;
+			this->Precio->Name = L"Precio";
+			this->Precio->ReadOnly = true;
+			this->Precio->Width = 250;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(827, 232);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(178, 52);
+			this->button3->TabIndex = 7;
+			this->button3->Text = L"Eliminar";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// registros
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(16, 31);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1175, 742);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Name = L"registros";
+			this->Text = L"registros";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
